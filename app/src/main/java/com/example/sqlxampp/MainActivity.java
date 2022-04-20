ac8ee4238d370fcbeebd7d9e15d7dee196b686bb
miftahjnation <62986167+miftahjnation@users.noreply.github.com>
2022-04-21 00:54:35 +0700

    Menambahkan Inmpelemnt pada build.gradle, menambahkan activity Tambah teman serta mengatur layout yang diinginkan

--- app/src/main/java/com/example/sqlxampp/MainActivity.java
+++ app/src/main/java/com/example/sqlxampp/MainActivity.java
@@ -6,12 +6,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.sqlxampp.adapter.TemanAdapter;
 import com.example.sqlxampp.database.DBController;
 import com.example.sqlxampp.database.Teman;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,10 +31,13 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private TemanAdapter adapter;
-    private ArrayList<Teman> temanArrayList;
-    DBController controller = new DBController(this);
-    String id,nm,tlp;
+    private ArrayList<Teman> temanArrayList = new ArrayList<>();
     private FloatingActionButton fab;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static String url_select = "http://127.0.0.1/umyTI/bacateman.php";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAMA = "nama";
+    private static final String TAG_TELPON = "telpon";
 
 
     @Override
@@ -49,18 +64,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void BacaData(){
-        ArrayList<HashMap<String,String>> daftarTeman = controller.getAllTeman();
-        temanArrayList = new ArrayList<>();
-        //memindah dari hasil query kedalam Teman java Class
-        for (int i = 0; i<daftarTeman.size();i++)
-        {
-            Teman teman = new Teman();
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
 
-            teman.setId(daftarTeman.get(i).get("id").toString());
-            teman.setNama(daftarTeman.get(i).get("nama").toString());
-            teman.setTelpon(daftarTeman.get(i).get("telpon").toString());
-            //pindahkan dari teman ke dalam ArrayList teman di adapter
-            temanArrayList.add(teman);
-        }
+        JsonArrayRequest jArr = new JsonArrayRequest(url_select, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d(TAG, response.toString());
+
+                //parsing json
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject obj = response.getJSONObject(i);
+                        Teman item = new Teman();
+                        item.setId(obj.getString(TAG_ID));
+                        item.setNama(obj.getString(TAG_NAMA));
+                        item.setTelpon(obj.getString(TAG_TELPON));
+                        //menambahkan item ke array
+                        temanArrayList.add(item);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error" + error.getMessage());
+                error.printStackTrace();
+                Toast.makeText(MainActivity.this, "gagal", Toast.LENGTH_SHORT).show();
+            }
+        });
+        requestQueue.add(jArr);
     }
 }
