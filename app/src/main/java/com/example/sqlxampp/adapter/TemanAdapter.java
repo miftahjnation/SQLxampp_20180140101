a9cc11d2356ae73a7213a3a424c88e03d6fa24bb
miftahjnation <62986167+miftahjnation@users.noreply.github.com>
2022-05-22 05:16:10 +0800

    Menambahkan TemanAdapter.java dan method Hapus Data untuk menghapus data

--- app/src/main/java/com/example/sqlxampp/adapter/TemanAdapter.java
+++ app/src/main/java/com/example/sqlxampp/adapter/TemanAdapter.java
@@ -1,16 +1,38 @@
 package com.example.sqlxampp.adapter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.sqlxampp.EditTeman;
+import com.example.sqlxampp.MainActivity;
 import com.example.sqlxampp.R;
+import com.example.sqlxampp.app.AppController;
 import com.example.sqlxampp.database.Teman;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
     private ArrayList<Teman> listData;
@@ -29,8 +51,9 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
 
     @Override
     public void onBindViewHolder(TemanViewHolder holder, int position) {
-        String nm,tlp;
+        String id,nm,tlp;
 
+        id = listData.get(position).getId();
         nm = listData.get(position).getNama();
         tlp = listData.get(position).getTelpon();
 
@@ -39,6 +62,94 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
         holder.namaTxt.setText(nm);
         holder.telponTxt.setText(tlp);
 
+        holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                PopupMenu pm = new PopupMenu(view.getContext(), view);
+                pm.inflate(R.menu.popup1);
+                pm.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menu) {
+                        switch (menu.getItemId())
+                        {
+                            case R.id.edit:
+                                Bundle bendel = new Bundle();
+                                bendel.putString("kunci1", id);
+                                bendel.putString("kunci2", nm);
+                                bendel.putString("kunci3", tlp);
+                                Intent inten = new Intent(view.getContext(), EditTeman.class);
+                                inten.putExtras(bendel);
+                                view.getContext().startActivity(inten);
+                            case R.id.hapus:
+                                AlertDialog.Builder alertdb = new AlertDialog.Builder(view.getContext());
+                                alertdb.setTitle("Yakin "+nm+" akan dihapus?");
+                                alertdb.setMessage("Tekan Ya untuk menghapus");
+                                alertdb.setCancelable(false);
+                                alertdb.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int i) {
+                                        HapusData(id);
+                                        Toast.makeText(view.getContext(), "Data "+id+" telah dihapus", Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(view.getContext(), MainActivity.class);
+                                        view.getContext().startActivity(intent);
+
+                                    }
+                                });
+                                alertdb.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int i) {
+                                        dialog.cancel();
+                                    }
+                                });
+                                AlertDialog adlg = alertdb.create();
+                                adlg.show();
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                pm.show();
+                return true;
+            }
+        });
+
+    }
+
+    private void HapusData(final String idx)
+    {
+        String url_delete = "http://192.168.179.72/umyTI/deletetmn.php";
+        final String TAG = MainActivity.class.getSimpleName();
+        final String TAG_SUCCES = "success";
+        final int[] sukses = new int[1];
+
+        StringRequest stringReq = new StringRequest(Request.Method.POST, url_delete, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Respon : " + response.toString());
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    sukses[0] = jObj.getInt(TAG_SUCCES);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error : " + error.getMessage());
+
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("id", idx);
+
+                return params;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(stringReq);
     }
 
     @Override
